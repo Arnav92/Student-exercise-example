7da51b2484a957f5a1459710dfbb9a91c8b1ee16
Arnav <arnav.kumarltp@gmail.com>
2026-02-10 22:28:15 +0100

    done

--- Main.java
+++ Main.java
@@ -15,6 +15,15 @@ public class Main {
 
     // Placeholder for the linear sort method
     public static void linearSort(int[] array) {
-        // TODO: Add logic for linear sort
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    // Swap array[i] and array[j]
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
     }
 }
