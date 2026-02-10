a5a7953b14534f3ae979373b36f9f89bef9167d4
Arnav <arnav.kumarltp@gmail.com>
2026-02-10 22:22:49 +0100

    Base template

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Example array to sort
+        int[] numbers = {5, 2, 9, 1, 5, 6};
+
+        // TODO: Implement linear sort here
+        linearSort(numbers);
+
+        // Print sorted array
+        System.out.println("Sorted array:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+
+    // Placeholder for the linear sort method
+    public static void linearSort(int[] array) {
+        // TODO: Add logic for linear sort
     }
 }
